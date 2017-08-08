2d74c7d15db96e61dc09949cf05d9afcc7f04238
seufster <seufster@gmail.com>
2017-08-07 21:52:42 -0400

    init

---
+++ toasthub-social/src/main/java/org/toasthub/ToasthubSocialApplication.java
@@ -0,0 +1,12 @@
+package org.toasthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToasthubSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToasthubSocialApplication.class, args);
+	}
+}
